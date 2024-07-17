e1b73bbe87d305998cc554160be5a780ced866b6
Revathi-Kamineni <104614509+Revathi-Kamineni@users.noreply.github.com>
2024-07-17 17:54:24 +0530

    Update SeleniumTest.java

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -20,7 +20,7 @@ public class SeleniumTest {
         System.setProperty("webdriver.chrome.driver", "driver/chromedriver");//linux_64
 
         // Get file
-        File file = new File("TypeCoercion.html");
+        File file = new File("src/main/java/com/revature/index.html");
         String path = "file://" + file.getAbsolutePath();
 
         // Create a new ChromeDriver instance
